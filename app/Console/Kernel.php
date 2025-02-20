aa2d21d7f202c096f8277c5631ad80284663b43c
Strory16 <auliyaazzahraazzahra@gmail.com>
2025-02-20 13:36:36 +0700

    Karnel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,11 +1,17 @@
 <?php
 
+// Mendefinisikan namespace untuk kelas ini, yang membantu dalam mengorganisir kode dan menghindari konflik nama
 namespace App\Console;
 
+// Mengimpor kelas Schedule dari Laravel untuk penjadwalan tugas
 use Illuminate\Console\Scheduling\Schedule;
+
+// Mengimpor kelas ConsoleKernel dari Laravel, yang merupakan kelas dasar untuk kernel konsol
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+// Mendefinisikan kelas Kernel yang mewarisi dari ConsoleKernel
 class Kernel extends ConsoleKernel
+
 {
     /**
      * Define the application's command schedule.
@@ -27,6 +33,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require base_path ('routes/console.php');
     }
 }
